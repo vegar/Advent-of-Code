c7c0ead71024f6edb39fe8728895a2c51fe24cae
vegar <vegar@vi-kan.net>
2017-12-01 22:32:16 +0100

    2016 - day 1

--- index.js
+++ index.js
@@ -9,5 +9,72 @@ function captcha(input, next = (i, len) => i+1) {
 	return output;
 }
 
+function distance(to, from = {x: 0, y: 0}) {
+	return Math.abs(to.x - from.x) + Math.abs(to.y-from.y);
+}
 
-export { captcha }
+function navigate(input, callback = (from, to) => {}) {
+	const north = (pos, steps) => { return { x: pos.x, y: pos.y + steps } };
+	const south = (pos, steps) => { return { x: pos.x, y: pos.y - steps } };
+	const east = (pos, steps) => { return { x: pos.x + steps, y: pos.y } };
+	const west = (pos, steps) => { return { x: pos.x - steps, y: pos.y } };
+	const allDirections = [north, east, south, west];
+
+	let turn = (from, dir) => {
+		let f = {
+			R: (r) => (r+1) % 4,
+			L: (r) => (4+r-1) % 4
+		};
+
+		let result = f[dir](allDirections.indexOf(from));
+		return allDirections[result];
+	}
+
+	return input.split(', ').reduce(function(acc, curr) {
+		let move = turn(acc.dir, curr[0]);
+
+		let steps = parseInt(curr.slice(1), 10);
+
+		let newPos = move(acc, steps);
+		callback(acc, newPos);
+	  	return Object.assign({ dir: move }, newPos);
+	}, {x: 0, y: 0, dir: north});
+}
+
+function _intersects(a,b,c,d,p,q,r,s) {
+  var det, gamma, lambda;
+  det = (c - a) * (s - q) - (r - p) * (d - b);
+  if (det === 0) {
+    return false;
+  } else {
+    lambda = ((s - q) * (r - a) + (p - r) * (s - b)) / det;
+    gamma = ((b - d) * (r - a) + (c - a) * (s - b)) / det;
+    return (0 < lambda && lambda < 1) && (0 < gamma && gamma < 1);
+  }
+};
+
+function range([start, stop]) {
+	let result = [];
+	if (start != stop) {
+		let d = start > stop ? -1 : 1;
+
+		start += d;
+		while (d > 0 ? stop >= start : stop <= start) {
+	        result.push(start);
+	        start += d;
+	    }
+	}
+
+    return result;
+}
+
+function trace(from, to) {
+	let rangeSelector = (from, to) => from.y == to.y ? [from.x, to.x] : [from.y, to.y];
+	let mapper = (value) => from.y == to.y ? {x: value, y: from.y} : {x: from.x, y: value};
+
+	return range(rangeSelector(from, to))
+		.map(mapper);
+}
+
+
+export { captcha, distance, navigate, trace }
---
+++ test/2016-01.js
@@ -0,0 +1,73 @@
+import {distance, navigate, trace} from '../index.js';
+
+let assert = require('assert');
+
+describe('2016 - Day 1', function() {
+
+  let input = "L1, R3, R1, L5, L2, L5, R4, L2, R2, R2, L2, R1, L5, R3, L4, L1, L2, R3, R5, L2, R5, L1, R2, L5, R4, R2, R2, L1, L1, R1, L3, L1, R1, L3, R5, R3, R3, L4, R4, L2, L4, R1, R1, L193, R2, L1, R54, R1, L1, R71, L4, R3, R191, R3, R2, L4, R3, R2, L2, L4, L5, R4, R1, L2, L2, L3, L2, L1, R4, R1, R5, R3, L5, R3, R4, L2, R3, L1, L3, L3, L5, L1, L3, L3, L1, R3, L3, L2, R1, L3, L1, R5, R4, R3, R2, R3, L1, L2, R4, L3, R1, L1, L1, R5, R2, R4, R5, L1, L1, R1, L2, L4, R3, L1, L3, R5, R4, R3, R3, L2, R2, L1, R4, R2, L3, L4, L2, R2, R2, L4, R3, R5, L2, R2, R4, R5, L2, L3, L2, R5, L4, L2, R3, L5, R2, L1, R1, R3, R3, L5, L2, L2, R5"
+
+  describe('Part 1 - distance', function() {
+
+    it('Following R2, L3 leaves you 2 blocks East and 3 blocks North, or 5 blocks away.', function() {
+      let pos = navigate('R2, L3');
+      assert.equal(5, distance(pos));
+    });
+
+    it('R2, R2, R2 leaves you 2 blocks due South of your starting position, which is 2 blocks away.', function() {
+      let pos = navigate('R2, R2, R2')
+      assert.equal(2, distance(pos));
+    });
+
+    it('R5, L5, R5, R3 leaves you 12 blocks away.', function() {
+      let pos = navigate('R5, L5, R5, R3');
+      assert.equal(12, distance(pos));
+    });
+
+    it('How many blocks away is Easter Bunny HQ? -> 278', function() {
+      let pos = navigate(input);
+      assert.equal(278, distance(pos));
+    });
+
+  });
+
+  describe('Part 2 - revisit', function() {
+    it('if your instructions are R8, R4, R4, R8, the first location you visit twice is 4 blocks away, due East', function(){
+
+      let path = [ {x: 0, y: 0} ];
+      let intersection = null;
+
+      let pos = navigate('R8, R4, R4, R8', (from, to) => {
+        var tracks = trace(from, to);
+
+        tracks.forEach(pos => {
+          if (path.find((oldPos) => pos.x == oldPos.x && pos.y == oldPos.y)) {
+            intersection = intersection || pos;
+          }
+        });
+        path.push(...tracks);
+      });
+
+      assert.equal(4, distance(intersection || pos));
+    });
+
+    it('How many blocks away is the first location you visit twice? -> 161', function(){
+
+      let path = [ {x: 0, y: 0} ];
+      let intersection = null;
+
+      let pos = navigate(input, (from, to) => {
+        var tracks = trace(from, to);
+
+        tracks.forEach(pos => {
+          if (path.find((oldPos) => pos.x == oldPos.x && pos.y == oldPos.y)) {
+            intersection = intersection || pos;
+          }
+        });
+        path.push(...tracks);
+      });
+
+      assert.equal(161, distance(intersection || pos));
+    });
+  });
+
+});
