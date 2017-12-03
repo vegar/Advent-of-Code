74b05f7f3bc2b630d4cd47a76baacedc8c038cf8
vegar <vegar@vi-kan.net>
2017-12-03 22:15:48 +0100

    2017 - Day 3

---
+++ test/2017-03.js
@@ -0,0 +1,81 @@
+import {distance, indexToCoord, coordToIndex, adjecent, sumAdjecent} from '../lib';
+
+let assert = require('chai').assert;
+
+describe('2017 - Day 3', function() {
+
+  let input = 361527;
+
+  describe('Part 1', function() {
+    it(`Data from square 1 is carried 0 steps, since it's at the access port.`, function() {
+      let coord = indexToCoord(1);
+
+      assert.equal(0, distance(coord));
+    });
+
+    it(`Data from square 12 is carried 3 steps, such as: down, left, left.`, function() {
+      let coord = indexToCoord(12);
+
+      assert.equal(3, distance(coord));
+    });
+
+    it(`Data from square 23 is carried only 2 steps: up twice.`, function() {
+      let coord = indexToCoord(23);
+
+      assert.equal(2, distance(coord));
+    });
+
+    it(`Data from square 1024 must be carried 31 steps.`, function() {
+      let coord = indexToCoord(1024);
+
+      let distance = Math.abs(coord[0]) + Math.abs(coord[1]);
+
+      assert.equal(31, distance);
+    });
+
+    it('How many steps are required to carry the data from the square identified in your puzzle input all the way to the access port? --> 326', function(){
+      let coord = indexToCoord(input);
+
+      assert.equal(326, distance(coord));
+    });
+  });
+
+  describe('Part 2', function() {
+
+    it('Square 1 starts with the value 1.', function() {
+      assert.equal(1, sumAdjecent(1, []));
+    });
+
+    it('Square 2 has only one adjacent filled square (with value 1), so it also stores 1.', function() {
+      assert.equal(1, sumAdjecent(2, [1]));
+    });
+
+    it('Square 3 has both of the above squares as neighbors and stores the sum of their values, 2.', function() {
+      assert.equal(2, sumAdjecent(3, [1, 1]));
+    });
+
+    it('Square 4 has all three of the aforementioned squares as neighbors and stores the sum of their values, 4.', function() {
+      assert.equal(4, sumAdjecent(4, [1, 1, 2]));
+    });
+
+    it('Square 5 only has the first and fourth squares as neighbors, so it gets the value 5.', function() {
+      assert.equal(5, sumAdjecent(5, [1, 1, 2, 4]));
+    });
+
+    it('What is the first value written that is larger than your puzzle input? --> 363010 ', function() {
+      let sums = [];
+      let idx = 1;
+      let sum;
+      while(true) {
+        sum = sumAdjecent(idx, sums);
+        sums.push(sum);
+        idx++;
+
+        if (sum > input)
+          break;
+      }
+
+      assert.equal(363010, sum);
+    });
+  });
+});
---
+++ test/spiralMatrixTests.js
@@ -0,0 +1,34 @@
+import {distance, indexToCoord, coordToIndex, adjecent, sumAdjecent} from '../lib';
+
+let assert = require('chai').assert;
+
+describe('adjecent', function() {
+  it('adjecent returns all adjecent coords', function(){
+    let adj = adjecent({ x:0, y:0});
+
+    assert.sameDeepMembers(
+      [
+        {x: -1, y:  1}, {x: 0, y:  1}, {x: 1, y:  1},
+        {x: -1, y:  0},                {x: 1, y:  0},
+        {x: -1, y: -1}, {x: 0, y: -1}, {x: 1, y: -1},
+      ],
+      adj);
+  });
+
+});
+
+describe('coordToIndex', function() {
+    let testCases = [
+      {expected: 1, coord: {x: 0, y: 0}},
+      {expected: 2, coord: {x: 1, y: 0}},
+      {expected: 3, coord: {x: 1, y: 1}},
+      {expected: 4, coord: {x: 0, y: 1}},
+    ]
+
+    testCases.forEach(function(testCase) {
+      it(`index for ${testCase.coord.x}:${testCase.coord.y} should be ${testCase.expected}`, function(){
+        assert.equal(testCase.expected, coordToIndex(testCase.coord));
+      })
+    });
+
+});
