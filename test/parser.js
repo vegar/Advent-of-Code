b3e947f331307247683583ea3cf37fe92b523d8e
vegar <vegar@vi-kan.net>
2017-12-08 22:06:25 +0100

    2017 - Day 8

---
+++ test/parser.js
@@ -0,0 +1,34 @@
+import {parser} from '../lib';
+
+let assert = require('chai').assert;
+
+describe('parser', function() {
+	it('parses b inc 5 if a > 1', function() {
+      let parsed = parser('b inc 5 if a > 1');
+
+      assert.deepEqual(parsed, {
+        addr: 'b',
+        op: 'inc',
+        value: 5,
+        ifAddr: 'a',
+        ifOp: '>',
+        ifValue: 1
+      });
+
+    });
+
+    it('parses c inc -20 if c == 10', function() {
+      let parsed = parser('c inc -20 if c == 10');
+
+      assert.deepEqual(parsed, {
+        addr: 'c',
+        op: 'inc',
+        value: -20,
+        ifAddr: 'c',
+        ifOp: '==',
+        ifValue: 10
+      });
+
+    });
+
+});
