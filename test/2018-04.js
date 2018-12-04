b333530a70931a1cafbe73984094445c4d0ea383
Vegar Vikan <vegar.vikan@visma.com>
2018-12-04 13:14:12 +0100

    2018 - Day 4

---
+++ test/2018-04.js
@@ -0,0 +1,102 @@
+import {readFileLines, lines} from '../lib';
+
+let assert = require('assert');
+
+describe.only('2018 - Day 4', function() {
+
+  let input;
+  before(() => input = readFileLines('./input/2018-04.txt'));
+
+  describe('Part 1', function() {
+
+    it('Find the guard that has the most minutes asleep. What minute does that guard spend asleep the most?', function() {
+      const regGuard = /:(\d\d).*(#(\d+)|falls|wakes)/;
+      let currentGuard = undefined;
+      let result = {};
+      input
+        .sort()
+        .forEach(line => {
+          const match = line.match(regGuard);
+          if (match[2] === 'falls') {
+            result[currentGuard].startSleep = match[1];
+          }
+          else if (match[2] === 'wakes') {
+            let awakes = match[1];
+            for (let i = result[currentGuard].startSleep; i < awakes; i++)
+              result[currentGuard].minutes[i] = (result[currentGuard].minutes[i] || 0) + 1;
+
+            result[currentGuard].startSleep = undefined;
+
+            result[currentGuard].sum = result[currentGuard].minutes.reduce((acc, curr) => (acc+curr));
+          }
+          else {
+            currentGuard = match[3];
+
+            if (!result[currentGuard]) {
+              result[currentGuard] = {
+                minutes: new Array(60),
+                sum: 0
+              };
+            }
+          }
+        });
+
+
+      let r = Object.entries(result).reduce((acc, curr) => curr[1].sum > acc[1].sum ? curr : acc);
+
+      let minute = r[1].minutes.reduce((acc, curr, idx) => (curr > acc.max ? { max: curr, idx} : acc),  {max: -1, idx: -1});
+
+      assert.equal(r[0]*minute.idx, 84834);
+    });
+  });
+
+  describe('Part 2', function() {
+
+    it('Of all guards, which guard is most frequently asleep on the same minute?', function() {
+      const regGuard = /:(\d\d).*(#(\d+)|falls|wakes)/;
+      let currentGuard = undefined;
+      let result = {};
+      input
+        .sort()
+        .forEach(line => {
+          const match = line.match(regGuard);
+          if (match[2] === 'falls') {
+            result[currentGuard].startSleep = match[1];
+          }
+          else if (match[2] === 'wakes') {
+            let awakes = match[1];
+            for (let i = result[currentGuard].startSleep; i < awakes; i++)
+              result[currentGuard].minutes[i] = (result[currentGuard].minutes[i] || 0) + 1;
+
+            result[currentGuard].startSleep = undefined;
+
+            result[currentGuard].max = result[currentGuard].minutes.reduce((acc, curr, idx, minutes) => (curr > (minutes[acc] || -1) ? idx : acc), -1);
+          }
+          else {
+            currentGuard = match[3];
+
+            if (!result[currentGuard]) {
+              result[currentGuard] = {
+                minutes: new Array(60),
+                max: 0
+              };
+            }
+          }
+        });
+
+        //console.log(JSON.stringify(result));
+
+        var guard = Object.entries(result).reduce((acc, curr) => {
+          console.log(`ACC: ${acc[0]} - ${acc[1].minutes[acc[1].max]}`);
+          console.log(`CURR: ${curr[0]} - ${curr[1].minutes[curr[1].max]}`);
+
+          if (!acc[1].max) return curr;
+
+          return curr[1].minutes[curr[1].max] > acc[1].minutes[acc[1].max] ? curr : acc;
+        });
+
+      //console.log(JSON.stringify(guard));
+      assert.equal(guard[0] * guard[1].max, 53427);
+    });
+  });
+});
