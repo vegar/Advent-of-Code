1aeef5901478eaf07bc989a3a0697e6671c16159
Vegar Vikan <vegar@vi-kan.net>
2019-12-03 08:50:26 +0100

    extract intcode from 2019-02

--- test/2019-02.js
+++ test/2019-02.js
@@ -1,50 +1,30 @@
-import {readFileAsNumbers} from '../lib';
+import {readFileAsNumbers, executeIntCode} from '../lib';
 import {assert} from 'chai';
 
-describe('2019 - Day 01', function() {
-
-  const execute = (program) => {
-    let p = [...program];
-    let position = 0;
-    while (p[position] !== 99) {
-      if (p[position] === 1) {
-        const a = p[p[position+1]];
-        const b = p[p[position+2]];
-        p[p[position+3]] = a + b;
-        position += 4;
-      }
-      if (p[position] === 2) {
-        const a = p[p[position+1]];
-        const b = p[p[position+2]];
-        p[p[position+3]] = a * b;
-        position += 4;
-      }
-    }
-    return p;
-  }
+describe('2019 - Day 02', function() {
 
   describe('Part 1', function() {
     it('1,0,0,0,99 becomes 2,0,0,0,99 (1 + 1 = 2).', () => {
-      const newProgram = execute([1,0,0,0,99]);
-      console.log(newProgram);
+      const newProgram = executeIntCode([1,0,0,0,99]);
+
       assert.sameOrderedMembers(newProgram, [2,0,0,0,99]);
     });
 
     it('2,3,0,3,99 becomes 2,3,0,6,99 (3 * 2 = 6).', () => {
-      const newProgram = execute([2,3,0,3,99]);
-      console.log(newProgram);
+      const newProgram = executeIntCode([2,3,0,3,99]);
+
       assert.sameOrderedMembers(newProgram, [2,3,0,6,99]);
     });
 
     it('2,4,4,5,99,0 becomes 2,4,4,5,99,9801 (99 * 99 = 9801).', () => {
-      const newProgram = execute([2,4,4,5,99,0]);
-      console.log(newProgram);
+      const newProgram = executeIntCode([2,4,4,5,99,0]);
+
       assert.sameOrderedMembers(newProgram, [2,4,4,5,99,9801]);
     });
 
     it('1,1,1,4,99,5,6,0,99 becomes 30,1,1,4,2,5,6,0,99.', () => {
-      const newProgram = execute([1,1,1,4,99,5,6,0,99]);
-      console.log(newProgram);
+      const newProgram = executeIntCode([1,1,1,4,99,5,6,0,99]);
+
       assert.sameOrderedMembers(newProgram, [30,1,1,4,2,5,6,0,99]);
     });
 
@@ -53,7 +33,7 @@ describe('2019 - Day 01', function() {
       program[1] = 12;
       program[2] = 2;
 
-      let newProgram = execute(program);
+      let newProgram = executeIntCode(program);
       assert.equal(newProgram[0], 3306701);
     });
   });
@@ -68,7 +48,7 @@ describe('2019 - Day 01', function() {
         program[1] = noun;
         program[2] = verb;
 
-        const newProgram = execute(program);
+        const newProgram = executeIntCode(program);
         result = newProgram[0];
         if (result !== 19690720)  {
           noun += 1;
